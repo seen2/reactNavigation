98b3af13c711e5d0481cc4aa4fb1e6955af24063
seen2 <tiwarietc@gmail.com>
2020-09-01 22:42:24 +0530

    added menu icon

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, SafeAreaView, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+function CustomHeader() {
+  return (
+    <View
+      style={{
+        // flex: 1,
+        height: 40,
+        width: "100%",
+        flexDirection: "row",
+      }}
+    >
+      <View style={{ flex: 1 }}>
+        <Image
+          source={require("./src/assets/menu.png")}
+          style={{ height: 40, width: 40, marginLeft: 5 }}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={{ flex: 1.5 }}></View>
+      <View style={{ flex: 1 }}></View>
+    </View>
+  );
+}
+
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, paddingTop: 30 }}>
+      <CustomHeader />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Home!</Text>
+        <Button title="Go to HomeDetails" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function HomeScreenDetails() {
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: 30 }}>
+      <CustomHeader />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Home Screen Details!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, paddingTop: 30 }}>
+      <CustomHeader />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Settings</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
